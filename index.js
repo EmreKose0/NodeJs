de5ad4c79f5f273183065bb80cb6dbb68b6561ab
EmreKose0 <emrekose1995@gmail.com>
2024-07-14 03:54:04 -0700

    lv2

--- index.js
+++ index.js
@@ -1,31 +1,31 @@
-var http = require("http");     //for respond & response, need to import http module
-var fs = require("fs");         //read document, need to import fs module
+const express = require("express");
+const app = express();
+app.set("view engine","ejs");  // using ejs for view engine
+app.use(express.static('public'));  //to access to public file img, js, css etc.
+app.use(express.static('node_modules'));  //to access to the node_modules for using bootstrap
 
-var server = http.createServer((req,res) => {
+const data = [                                  //Create list 
+    {id:1, name:"iphone14", price:30000, imgUrl:"1.jpeg"},
+    {id:2, name:"iphone15", price:40000, imgUrl:"2.jpeg"},
+    {id:3, name:"iphone16", price:50000, imgUrl:"3.jpeg"},
+    {id:4, name:"iphone17", price:60000, imgUrl:"4.jpeg"}
+]
 
-    if (req.url == "/"){
-        fs.readFile("index.html", (err,html) => {  //index.html den err ve html dönecek
-            res.write(html);
-            res.end();  //en sonda mutlaka res.end() denir. response u sonlandırmak içim
-        });
-
-    }else if (req.url == "/products"){
-        fs.readFile("urunler.html", (err,html) => {
-            res.write(html);
-            res.end();  //en sonda mutlaka res.end() denir. response u sonlandırmak içim
-        });
-
-    }else{
-        fs.readFile("404.html", (err,html) => {
-            res.write(html);
-            res.end();  //en sonda mutlaka res.end() denir. response u sonlandırmak içim
-        });
-
-    }
+app.use("/products/:id", function(req,res){ // for /, function gonna send homepage
+    const productID = data.find(p => p.id == req.params.id)   //find the id of item  
+    res.render("productDetails",productID);           //for send to info to the productdetails
 });
 
-server.listen(3000, () => {     //For listening port
-
-    console.log("node.js server at port 3000");
-
+app.use("/products", function(req,res){ // for /, function gonna send homepage
+    res.render("products",{                 
+        product: data                   //send data like object
+    });
 });
+
+app.use("/", function(req,res){ // for /, function gonna send homepage
+    res.render("index");
+});
+
+app.listen(3000, () =>{      //for descb listening port
+    console.log("listening on port 3000");
+})
